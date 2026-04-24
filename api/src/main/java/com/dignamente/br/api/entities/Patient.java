e29a60d38280f0996f2bc1d560d38ff29e13226a
klayveralencar777 <alencar.klayver7@gmail.com>
2026-04-24 19:13:47 -0300

    fix: id entity

--- api/src/main/java/com/dignamente/br/api/entities/Patient.java
+++ api/src/main/java/com/dignamente/br/api/entities/Patient.java
@@ -1,13 +1,10 @@
 package com.dignamente.br.api.entities;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -23,9 +20,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Patient extends User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    
 
     @Column(nullable = false)
     @NotBlank(message = "O cartão SUS é obrigatório")
--- api/src/main/java/com/dignamente/br/api/entities/Psychologist.java
+++ api/src/main/java/com/dignamente/br/api/entities/Psychologist.java
@@ -1,13 +1,11 @@
 package com.dignamente.br.api.entities;
 
 import java.time.LocalDate;
-import java.util.UUID;
+
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -23,9 +21,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Psychologist extends User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    
 
     @Column(nullable = false, unique = true)
     @NotBlank(message = "O CRP é obrigatório")
