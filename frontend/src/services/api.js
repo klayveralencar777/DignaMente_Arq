b6e387f045980bd9a970cade8975bd529394bdc0
Jatanael <jatanaeldantaasfaculdade@gmail.com>
2026-07-27 15:50:15 -0300

    Remove comment in api.js
    Removed comment about the back-end address.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// Aqui fica o endereço que o back-end Java está rodando
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
 });
